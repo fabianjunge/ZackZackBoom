6176d2a14733cbbc8a99944a0835a792f55db35a
faBimon <fabian@Fabians-Macbook.local>
2013-09-28 10:21:20 +0200

    Javascript Source outsourced

---
+++ public/javascript/client.js
@@ -0,0 +1,27 @@
+var socket = io.connect('http://192.168.2.135:3000');
+
+socket.on('bombe', function (data) {
+  console.log("Explodiere!");
+  var instance = createjs.Sound.play("boom");
+  instance.setMute(false);
+  instance.volume = 0.5;
+})
+
+socket.on('info', function (data) {
+  console.log("Player: " + data.connections_cnt + ", Bombs: " + data.bombs_cnt);
+})
+
+createjs.Sound.setMute(false);
+createjs.Sound.registerSound("/sounds/bomb.mp3", "boom");
+
+function booooom() {
+  socket.emit('explodiere', { my: 'data' });
+}
+
+$(document).ready( function() {
+  $(".button").click( function() {
+    var instance = createjs.Sound.play("boom");
+    instance.setMute(false);
+    instance.volume = 0.5;
+  });
+});
