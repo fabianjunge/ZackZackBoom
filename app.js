af45be50b4f08770aa1df18de9b55237df638a9b
Lasse Schwarten <lassefs@web.de>
2013-09-28 15:48:30 +0200

    it works … kind of

--- app.js
+++ app.js
@@ -39,18 +39,15 @@ var bombs = [];
 
 io.sockets.on('connection', function (socket) {
   connections.push(socket);
-  
-  setInterval(function(){
-    socket.emit('info', { connections_cnt: connections.length, bombs_cnt: bombs.length});
-  }, 2000);
-  
-  socket.on('explodiere', function (data) {
-      // Broadcast explodiere
-	  for (var i = 0; i < connections.length; i++) {
-		  connections[i].emit('bombe', { hello: 'world' });
-		  console.log("An Socket " + i + "gesendert, dass er explodieren soll.");
-	  }
-  });
+
+  // create bomb if more then two players are connected and we don't have one already
+  if(connections.length > 1 && bombs.length == 0) {
+    var ttl = 20 //Math.floor(Math.random()*100)
+    bombs.push({id: bombs.length, ttl: ttl, handlerId: socket.id })
+    socket.emit('caughtBomb');
+  } else {
+    socket.emit('lostBomb');
+  }
 
   socket.on('disconnect', function () {
     for (var i = 0; i < connections.length; i++) {
@@ -61,8 +58,67 @@ io.sockets.on('connection', function (socket) {
       }
     }
   });
+
+  socket.on('throwBomb', function () {
+    console.log("throwBomb " + socket.id );
+    var victim = getRandomConnectionExceptMe(socket);
+    bombs[0].handlerId = victim.id;
+    victim.emit('caughtBomb');
+    socket.emit('lostBomb');
+  });
 });
 
+setInterval(function(){
+  // calculate bomb ttl
+  var bomb_ttl = -1;
+  var bomb_holder = "none";
+  if(bombs.length > 0) {
+    bombs[0].ttl = bombs[0].ttl - 1;
+    bomb_ttl = bombs[0].ttl;
+    bomb_holder = bombs[0].handlerId;
+  }
+  // inform all clients about game state
+  for (var i = 0; i < connections.length; i++) {
+    connections[i].emit('info', { connections_cnt: connections.length, bombs_cnt: bombs.length, bombs_ttl: bomb_ttl, bomb_holder: bomb_holder});
+  }
+  // check if bomb goes boooooooom!
+  if(bombs.length > 0 && bombs[0].ttl < 1) {
+    getSocketById(bombs[0].handlerId).emit('explodeBomb');
+    bombs.pop();
+  }
+}, 1000);
+
+var getSocketNrById = function(id) {
+  for (var i = 0; i < connections.length; i++) {
+    if(id == connections[i].id) {
+      return i;
+    }
+  }
+}
+
+var getSocketById = function(id) {
+  return connections[getSocketNrById(id)];
+}
+
+var getRandomConnectionExceptMe = function(socket) {
+  var victim_nr = randomFromMinMax(0,(connections.length - 1) - 1);
+  var myNr = getSocketNrById(socket.id);
+  console.log('myNr: ' + myNr);
+  if (victim_nr >= myNr) { victim_nr = victim_nr + 1;};
+  console.log('victim_nr: ' + victim_nr);
+  //var newArray = getConnectionsWithoutMe(socket);
+  return connections[victim_nr];
+}
+
+var getConnectionsWithoutMe = function(my_socket) {
+  var nr = getSocketNrById(my_socket.id);
+  return connections.slice(0,nr).concat(connections.slice(nr,-1));
+}
+
+var randomFromMinMax = function(min, max) {
+  var delta = (max - min) + 1;
+  return Math.floor(Math.random() * delta) + min;
+}
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,30 +1,51 @@
 var socket = io.connect('http://localhost:3000');
 
-socket.on('bombe', function (data) {
+function gotBomb() {
+  $(".bomb").show();
+  $(".nobomb").hide();
+  $(".button_active").show();
+  $(".button_inactive").hide();
+};
+
+function lostBomb() {
+  $(".bomb").hide()
+  $(".nobomb").show()
+  $(".button_active").hide()
+  $(".button_inactive").show()
+};
+
+socket.on('explodeBomb', function (data) {
   console.log("Explodiere!");
   var instance = createjs.Sound.play("boom");
   instance.setMute(false);
   instance.volume = 0.5;
-})
+  lostBomb();
+  alert("You've got bombed!!!")
+});
 
 socket.on('info', function (data) {
-  console.log("Player: " + data.connections_cnt + ", Bombs: " + data.bombs_cnt);
-  
+  console.log("Player: " + data.connections_cnt + ", Bombs: " + data.bombs_cnt + ", TTL: " + data.bombs_ttl + ", Holder: " + data.bomb_holder);
   $("#stats .player p").text(data.connections_cnt);
   $("#stats .bomb p").text(data.bombs_cnt);
-})
+});
+
+socket.on('lostBomb', function () {
+  lostBomb();
+});
+
+socket.on('caughtBomb', function () {
+  gotBomb();
+});
 
 createjs.Sound.setMute(false);
 createjs.Sound.registerSound("/sounds/bomb.mp3", "boom");
 
-function booooom() {
-  socket.emit('explodiere', { my: 'data' });
-}
-
 $(document).ready( function() {
-  $(".button").click( function() {
-    var instance = createjs.Sound.play("boom");
-    instance.setMute(false);
-    instance.volume = 0.5;
+  $(".button_active").click( function() {
+    //var instance = createjs.Sound.play("boom");
+    //instance.setMute(false);
+    //instance.volume = 0.5;
+    socket.emit('throwBomb');
+    console.log('sent throwBomb')
   });
 });
