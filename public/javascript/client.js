f3ac745124e2378e22b5ddabab2335a34dbd2fba
faBimon <fabian@Fabians-Macbook.local>
2013-09-29 11:43:06 +0200

    Fixed bug

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -68,7 +68,5 @@ $(document).ready( function() {
     $("#login_overlay").fadeOut();
     socket.emit('register', { name: $(".loginname").val() , email: "franz@aol.de"} );
     console.log($(".loginname").val());
-  })
-  
   });
 });
